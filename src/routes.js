007c0a381ce7a3be41bb97d5e192c8e10061c873
RayanLucas97 <contato.rayanlucas97@gmail.com>
2025-03-03 13:56:46 -0300

    errodo routes.js arrumado

--- src/routes.js
+++ src/routes.js
@@ -3,8 +3,10 @@ import UserController from "./app/controllers/UserController.js";
 
 const routes = Router();
 
+const userController = new UserController();
 
-routes.post('/users', UserController.store);
+
+routes.post('/users', userController.store);
 
 export default routes;
 
