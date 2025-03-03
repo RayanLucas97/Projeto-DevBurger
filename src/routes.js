6c6fb8caa77c0aca87e83bc16a93753256443855
RayanLucas97 <contato.rayanlucas97@gmail.com>
2025-03-03 12:44:17 -0300

    erro no routes.post

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,11 @@
 import { Router } from "express";
 import UserController from "./app/controllers/UserController.js";
 
-
 const routes = Router();
 
-routes.post("/users", (req, res) => {
-    UserController.store(req, res);
-});
 
+routes.post('/users', UserController.store);
 
 export default routes;
+
+
