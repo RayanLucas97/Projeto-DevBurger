0e7f82bc2052fe6c6d13ca24c32d8d28016e6170
RayanLucas97 <contato.rayanlucas97@gmail.com>
2025-03-04 11:11:44 -0300

    adicionado o yarn add yup na aplicação

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,10 +1,23 @@
 import User from "../controllers/models/User.js"
-
 import { v4 } from "uuid";
+import * as Yup from "yup"; 
+
 
 
 class UserController {
     async store (request, response){
+        const schema = Yup.object({
+            name: Yup.string().required(),
+            email: Yup.string().email().required(),
+            password_hash: Yup.string().min(6).required(),
+            admin: Yup.boolean(),
+        });
+
+       try{
+        schema.validateSync(request.body, {abortEarly: false});
+       }catch (err){
+        return response.status(400).json({ error: err.errors});
+       }
 
      const{name, email , password_hash ,admin} = request.body
 
