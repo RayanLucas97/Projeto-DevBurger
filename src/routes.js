fda58ca3309a0688b63f30b5483ca3005afdeebd
RayanLucas97 <contato.rayanlucas97@gmail.com>
2025-03-05 14:03:38 -0300

    adicionado o SessionController.js na aplicação

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,16 @@
 import { Router } from "express";
 import UserController from "./app/controllers/UserController.js";
+import SessionController from "./app/controllers/SessionController.js";
+
+
 
 const routes = Router();
-
 const userController = new UserController();
 
 
+
 routes.post('/users', userController.store);
+routes.post("/session", SessionController.store);
 
 export default routes;
 
